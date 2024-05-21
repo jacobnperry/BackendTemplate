2c1ddebd84e9c714c9a0de3acf5f1b8106c062ea
Jacob Perry <jacobnperry@gmail.com>
2024-05-21 08:25:19 -0700

    mm

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://mypantry-two.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://mypantry-two.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
