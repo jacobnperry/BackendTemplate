1981c409e10a734611f636f28195fb08b8d47a27
Jacob Perry <jacobnperry@gmail.com>
2024-05-21 08:14:17 -0700

    CORS update

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://mypantry-two.vercel.app/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
